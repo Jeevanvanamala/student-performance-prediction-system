2f5d0772cd3bd1068df328336fb21761883b2bfd
Jeevan Vanamala <jeevanvanamala17@gmail.com>
2026-06-05 01:43:17 +0530

    Fix CORS for Vercel frontend

--- src/main/java/com/student/performance/config/CorsConfig.java
+++ src/main/java/com/student/performance/config/CorsConfig.java
@@ -10,12 +10,17 @@ public class CorsConfig {
 
     @Bean
     public WebMvcConfigurer corsConfigurer() {
+
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
+
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+                        .allowedOrigins(
+                                "http://localhost:3000",
+                                "https://student-performance-frontend-one.vercel.app"
+                        )
+                        .allowedMethods("*")
                         .allowedHeaders("*");
             }
         };
